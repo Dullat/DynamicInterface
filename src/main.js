ca34a076fb6ce6d501c3e94bef504eaa48815897
dullat <dullat@users.noreply.github.com>
2024-04-30 23:40:21 +0530

    update

--- src/main.js
+++ src/main.js
@@ -1,2 +1,28 @@
 import './scss/style.scss';
 import './scss/reset.scss';
+
+const items = document.querySelectorAll(`[class*="carousel-item"]`);
+
+let slideIndex = 0;
+
+document.addEventListener('DOMContentLoaded', runSlider);
+
+function runSlider() {
+  items[slideIndex].classList.add('active');
+  setInterval(nextSlide, 4000);
+}
+
+function showSlide(i) {
+  items.forEach((item) => {
+    item.classList.remove('active');
+  });
+  items[i].classList.add('active');
+}
+
+function nextSlide() {
+  slideIndex += 1;
+  if (slideIndex >= items.length) {
+    slideIndex = 0;
+  }
+  showSlide(slideIndex);
+}
