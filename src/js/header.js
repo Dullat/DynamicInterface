e611b300df638581273af1b77dcca10dfec9949a
dullat <dullat@users.noreply.github.com>
2024-06-13 00:26:10 +0530

    update

--- src/js/header.js
+++ src/js/header.js
@@ -1,7 +1,13 @@
 const toggledMenuDropBtn = document.querySelector('.menu-toggle-btn');
 const toggledMenu = document.querySelector('.toggled-menu');
+const sideBarBtn = document.querySelector('.toggle-btn');
+const nav = document.querySelector('nav');
 
 // drop
+
+function preventScroll(event) {
+  window.scrollTo(0, 0);
+}
 
 function headerScript() {
   toggledMenuDropBtn.addEventListener('click', () => {
@@ -13,6 +19,18 @@ function headerScript() {
     if (e.target !== toggledMenu && e.target !== toggledMenuDropBtn) {
       toggledMenu.classList.remove('active');
       toggledMenuDropBtn.classList.remove('active');
+    }
+  });
+
+  sideBarBtn.addEventListener('click', () => {
+    if (nav.classList.contains('active')) {
+      nav.classList.remove('active');
+      sideBarBtn.classList.remove('active');
+      window.addEventListener('scroll', preventScroll);
+    } else if (!nav.classList.contains('active')) {
+      nav.classList.add('active');
+      sideBarBtn.classList.add('active');
+      window.removeEventListener('scroll', preventScroll);
     }
   });
 }
